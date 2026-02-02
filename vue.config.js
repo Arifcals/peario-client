c75a84d45867a0f4f69809aea5c655bf3a64b946
Arifcals <arif.calv@gmail.com>
2026-02-02 22:05:31 +0300

    initial peario client

--- vue.config.js
+++ vue.config.js
@@ -3,53 +3,61 @@ const webpack = require('webpack');
 const { version } = require('./package.json');
 
 module.exports = defineConfig({
-    configureWebpack: {
-        resolve: {
-            fallback: {
-                stream: false,
-                process: require.resolve('process/browser'),
-                buffer: require.resolve('buffer/'),
-                events: require.resolve('events'),
-                querystring: require.resolve('querystring-es3'),
-            }
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                Buffer: ['buffer', 'Buffer'],
-                process: 'process/browser',
-            }),
-            new webpack.DefinePlugin({
-                'process.env.PACKAGE_VERSION': JSON.stringify(version),
-            })
-        ],
-        devServer: {
-            client: {
-                overlay: false,
-            },
-        },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/peario/'
+    : '/',
+
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        stream: false,
+        process: require.resolve('process/browser'),
+        buffer: require.resolve('buffer/'),
+        events: require.resolve('events'),
+        querystring: require.resolve('querystring-es3'),
+      }
     },
-    css: {
-        loaderOptions: {
-            sass: {
-                additionalData: `
-                    @import "src/assets/styles/variables.scss";
-                `
-            }
-        }
-    },
-    chainWebpack: config => {
-        config.module
-            .rule('vue')
-            .use('vue-loader')
-            .tap(options => ({
-                ...options,
-                compilerOptions: {
-                    isCustomElement: (tag) => tag === 'ion-icon'
-                }
-            }));
-    },
-    transpileDependencies: [
-        'vue-meta',
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+        process: 'process/browser',
+      }),
+      new webpack.DefinePlugin({
+        'process.env.PACKAGE_VERSION': JSON.stringify(version),
+      })
     ],
-    productionSourceMap: false,
+    devServer: {
+      client: {
+        overlay: false,
+      },
+    },
+  },
+
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "src/assets/styles/variables.scss";
+        `
+      }
+    }
+  },
+
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => ({
+        ...options,
+        compilerOptions: {
+          isCustomElement: (tag) => tag === 'ion-icon'
+        }
+      }));
+  },
+
+  transpileDependencies: [
+    'vue-meta',
+  ],
+
+  productionSourceMap: false,
 });
